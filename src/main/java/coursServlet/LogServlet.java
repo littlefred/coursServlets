0628134a7e4d2bdb0267b4663765ece9168a03c8
Frederick <Frederick@192.168.43.248>
2018-05-07 16:00:05 +0200

    commit to push on github

--- src/main/java/coursServlet/LogServlet.java
+++ src/main/java/coursServlet/LogServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.ServletResponse;
 
 public class LogServlet extends GenericServlet {
 	private static final long serialVersionUID = 1L;
-	private List<String> log = Collections.synchronizedList(new ArrayList());
+	private List<String> log = Collections.synchronizedList(new ArrayList<>());
 
 	@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
